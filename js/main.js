5024905e29fd2290920511a6de587ab4102b91e5
flowwalker <2870541857@qq.com>
2026-07-20 20:20:41 +0800

    Site updated: 2026-07-20 20:20:41

--- js/main.js
+++ js/main.js
@@ -339,14 +339,6 @@
     });
   }
 
-  /* External links open in new tab */
-  document.querySelectorAll('.post-content a[href^="http"]').forEach(function(a) {
-    if (!a.href.includes(window.location.hostname)) {
-      a.setAttribute('target', '_blank');
-      a.setAttribute('rel', 'noopener noreferrer');
-    }
-  });
-
   /* Heading collapse/expand */
   var CHEVRON_SVG = '<svg class="heading-chevron" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 12 15 18 9"/></svg>';
   var postContent = document.querySelector('.post-content');
